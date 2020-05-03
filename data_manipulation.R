21c150bc9ecf555f8898018045f17d091d9a1546
Gabriel Pena <gpena@untref.edu.ar>
2020-05-03 16:20:06 -0300

    Fixed beginning of the fitting on datasets starting with zeros

--- data_manipulation.R
+++ data_manipulation.R
@@ -1,5 +1,11 @@
 library(zoo)
 
 format_data_for_fitting <- function(data_vector){
-  fittable_vector <- as.vector(na.locf(data_vector))
+  data_vector <- remove_nas(data_vector)
+  first_nonzero_day <- min(which(data_vector > 0))
+  fittable_data <- data_vector[first_nonzero_day:length(data_vector)]
+}
+
+remove_nas <- function(data_vector){
+  data <- as.vector(na.locf(data_vector))
 }
