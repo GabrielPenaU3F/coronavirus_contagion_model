ecb8127e0298d3533ada3e3767179abf852424b8
Gabriel Pena <gpena@untref.edu.ar>
2020-05-03 16:31:49 -0300

    Changed parameter display to rho and gamma/rho

--- displayer.R
+++ displayer.R
@@ -1,9 +1,9 @@
 display_estimated_coefficients <- function(coefs){
-  a = coefs[[1]]
-  b = coefs[[2]]
+  rho = coefs[[1]]
+  gamma_over_rho = coefs[[2]]
   cat("\n\n--------------------\n\n")
   cat("Coefficients: \n\n")
-  cat(paste("a = ", a, "\n", sep="", collapse=NULL))
-  cat(paste("b = ", b, "\n", sep="", collapse=NULL))
+  cat(paste(intToUtf8(961), " = ", rho, "\n", sep="", collapse=NULL))
+  cat(paste(intToUtf8(947), "/", intToUtf8(961), " = ", gamma_over_rho, "\n", sep="", collapse=NULL))
   cat("\n--------------------\n\n")
 }
