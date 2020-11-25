6180bf8170dfdf257b4a4bfe9551e1fa5c5eeb9f
Gabriel Pena <gpena@untref.edu.ar>
2020-11-25 19:14:41 -0300

    Implemented general version of rsquared coefficient. Now it works properly

--- goodness_of_fit.R
+++ goodness_of_fit.R
@@ -1,11 +1,25 @@
 library(Metrics)
 
 calculate_determination_coefficient <- function(real_data, estimated_data){
-  data_variance <- var(real_data)
-  estimation_variance <- var(estimated_data)
-  estimation_variance / data_variance
+  sstot <- calculate_sstot(real_data)
+  ssres <- calculate_ssres(real_data, estimated_data)
+  rsq <- 1 - (ssres/sstot)
+  rsq
 }
 
 calculate_rrse <- function(real_data, estimated_data){
   rrse(real_data, estimated_data)
+}
+
+calculate_sstot <- function(real_data){
+  ybar <- mean(real_data)
+  squares <- (real_data - ybar)^2
+  sstot <- sum(squares)
+  sstot
+}
+
+calculate_ssres <- function(real_data, estimated_data){
+  residuals <- real_data - estimated_data
+  ssres <- sum(residuals^2)
+  ssres
 }
