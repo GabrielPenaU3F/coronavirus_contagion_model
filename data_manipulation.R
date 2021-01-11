b8aee8401972e1fc4cf18a6c2cb107b5a12c8115
Gabriel Pena <gpena@untref.edu.ar>
2021-01-11 00:20:48 -0300

    Slight refactor and code improvements. Rework to the show data from country function

--- data_manipulation.R
+++ data_manipulation.R
@@ -1,13 +1,8 @@
 library(zoo)
 
 format_data_for_fitting <- function(data_vector){
-  data_vector <- remove_nas(data_vector)
   first_nonzero_day <- min(which(data_vector > 0))
   fittable_data <- data_vector[first_nonzero_day:length(data_vector)]
-}
-
-remove_nas <- function(data_vector){
-  data <- as.vector(na.locf(data_vector))
 }
 
 create_mtbi_dataframe <- function(days, mtbis){
--- data_selection.R
+++ data_selection.R
@@ -6,14 +6,29 @@ show_availiable_countries <- function(){
 }
 
 get_data_from_country <- function(country, dataset){
-  country_data <- filter(full_dataset, location==country)
+  country_data <- filter_country_data(country, dataset)
   if (dataset == 'total_cases'){
-    data <- as.vector(select(country_data, total_cases)$total_cases)
+    data <- as.vector(country_data$total_cases)
   } else if (dataset == 'total_deaths'){
-    data <- as.vector(select(country_data, total_deaths)$total_deaths)
+    data <- as.vector(country_data$total_deaths)
   }
 }
 
-show_data_from_country <- function(country, dataset='total_cases'){
-  print(get_data_from_country(country, dataset))
+show_data_from_country <- function(country, dataset='total_cases', date=FALSE){
+  data <- filter_country_data(country, dataset)
+  if (date==FALSE) {
+    data <- subset(data, select=-c(date))
+  }
+  print(data)
+}
+
+filter_country_data <- function(country, dataset){
+  country_data <- filter(full_dataset, location==country)
+  if (dataset == 'total_cases'){
+    data <- select(country_data, date, total_cases)
+    data <- filter(data, !is.na(total_cases))
+  } else if (dataset == 'total_deaths'){
+    data <- select(country_data, date, total_deaths)
+    data <- filter(data, !is.na(total_deaths))  
+  }
 }
